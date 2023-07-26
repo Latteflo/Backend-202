0e9d9e302819236e245825f7a76ba93ae57fe9c5
Florentina <sf.simion.f@gmail.com>
2023-07-26 10:32:29 +0200

    corrected typo from sing to sign

--- express-jwt-tutorial/index.js
+++ express-jwt-tutorial/index.js
@@ -1,37 +1,60 @@
+// Load environment variables from .env file
 require("dotenv").config();
+
+// Import necessary modules
 const express = require("express");
 const jwt = require("jsonwebtoken");
 
+// Initialize a new express application
 const app = express();
 
+// Middleware for parsing JSON bodies from HTTP requests
 app.use(express.json());
 
-app.post("/singin", (req, res) => {
+// Route for signing in users
+app.post("/signin", (req, res) => {
+  // Extract email from request body
   const { email } = req.body;
+
+  // Create a user object
   const user = { email };
 
-  const accessToken = jws.sing(user, process.env.ACCESS_TOKEN_SECRET);
+  // Create an access token from the user object
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+
+  // Respond with the access token
   res.json({ accessToken });
 });
 
-
+// Middleware for authenticating tokens
 const authenticateToken = (req, res, next) => {
+  // Extract the authorization header
   const authHeader = req.headers["authorization"];
+
+  // Extract the token from the authorization header
   const token = authHeader && authHeader.split(" ")[1]; // Bearer TOKEN
 
+  // If there's no token, respond with 401 Unauthorized status code
   if (token == null) return res.sendStatus(401);
 
+  // Verify the token
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    // If the token is not valid, respond with 403 Forbidden status code
     if (err) return res.sendStatus(403);
+
+    // If the token is valid, add the user to the request and proceed to the next middleware
     req.user = user;
     next();
   });
 };
 
+// Route for accessing protected resources
 app.get("/protected", authenticateToken, (req, res) => {
+  // Respond with the email from the authenticated user
   res.json({ email: req.user.email });
 });
 
+// Start the server on port 3000
 app.listen(3000, () => {
   console.log("We are aliveee on port 3000");
 });
